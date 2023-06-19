1cf3557e217e8d54f5c0bdf348a1da1c7b1f7136
James-Frown <21122@ctca.co.za>
2023-06-19 21:55:28 +0200

    latest changes

--- chore-wise/src/app/page.tsx
+++ chore-wise/src/app/page.tsx
@@ -1,9 +1,27 @@
 import styles from './page.module.css'
 
 export default function Landing() {
+  await new Promise
   return (
-    <main className={styles.main}>
-      
-    </main>
+    <>
+      <main className={styles.main}>
+        <div>
+          <h1>
+            Welcome to Chore-Wise
+          </h1>
+          <p>
+            We are here to help you organise your chores!!!
+          </p>
+        </div>
+        <div>
+          <button>
+            Login
+          </button>
+          <button>
+            signup
+          </button>
+        </div>
+      </main>
+    </>
   )
 }
