abd4a9ee9073758f8631d9682b0b07016332ef19
James-Frown <21122@ctca.co.za>
2023-06-22 14:28:29 +0200

    latest changes | restarting project

---
+++ todo-wise/src/app/page.tsx
@@ -0,0 +1,25 @@
+import { Alert } from "@/components/bootstrap";
+
+export default function LandingPage() {
+  return (
+    <>
+      <div>
+        <div>
+          <Alert>
+            <h3>
+              Welcome to,
+            </h3>
+            <h1>
+              <strong> ChoreWise </strong>
+            </h1>
+            <span>
+            </span>
+            <p>
+              This is a smaple project where we encourgae childeren to do tasks for their parents in return for rewards!
+            </p>
+          </Alert>
+        </div>
+      </div>
+    </>
+  )
+}
